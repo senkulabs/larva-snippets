245f53301696d5fc4ccca609269e704839772b93
Kresna Satya <kresnasatya72@gmail.com>
2024-09-22 11:23:07 +0800

    create employee seeder

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -13,6 +13,27 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
-        Employee::factory()->count(10_000)->create();
+        $path = base_path('csvfile/employee_datatable.csv');
+
+        $file = fopen($path, 'r');
+
+        if ($file !== false) {
+            $header = array_map(function ($head) {
+                return implode('_', explode(' ', strtolower($head)));
+            }, fgetcsv($file));
+
+            $data = [];
+
+            if ($header !== false) {
+                while (($record = fgetcsv($file)) !== false) {
+                    $mapped_record = [];
+                    foreach ($record as $key => $value) {
+                        $mapped_record[$header[$key]] = $value;
+                    }
+                    array_push($data, $mapped_record);
+                }
+                Employee::insert($data);
+            }
+        }
     }
 }
