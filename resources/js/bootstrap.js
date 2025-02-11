1de34347c709596e9a1622626c71752c516c612b
Kresna Satya <kresnasatya72@gmail.com>
2025-02-12 06:58:58 +0800

    chore: remove axios

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,1 @@
-import axios from 'axios';
-window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name=csrf-token]').getAttribute('content');
