b4e0f841d0d1f5140c3d8b87aeded76e0946406b
Kresna Satya <kresnasatya72@gmail.com>
2025-08-01 21:39:41 +0800

    fix: tweak reset password

--- resources/views/livewire/reset-password.blade.php
+++ resources/views/livewire/reset-password.blade.php
@@ -82,13 +82,13 @@ class extends Component {
                 @error('password') <span class="text-red-500">{{ $message }}</span> @enderror
             </div>
         </div>
-        <button class="bg-blue-500 p-2 rounded text-white" wire:loading.attr="disabled">
+        <button class="bg-blue-500 p-2 rounded text-white cursor-pointer" wire:loading.attr="disabled">
             <svg version="1.1" class="h-5 w-5" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve" wire:loading wire:target="submit">
                 <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
                     <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite" />
                 </path>
             </svg>
-            Reset
+            Submit
         </button>
     </form>
 </div>
